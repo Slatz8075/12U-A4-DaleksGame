fc8cbada4020eb40cd6d65a254db33ca57ef3468
slatz8075 <slatz8075@HRH-IC0027332.wrdsb.ca>
2017-10-23 10:11:46 -0400

    More progress needed

--- src/CatchGame.java
+++ src/CatchGame.java
@@ -14,18 +14,15 @@ public class CatchGame {
      */
     //create the board 12 by 12
     Board b = new Board(12, 12);
-    //create the doctor at row 9 and column 9
-    Doctor Doctor = new Doctor(9,9);
-    
+    //create the doctor at a random row and column
+    Doctor Doctor = new Doctor((int)(Math.random()*12), (int)(Math.random()*12));
     //create an array for the daleks
-    
+    Dalek[] Daleks = new Dalek[2];
     //set the dalek's initial positions, at 3 random spots
-    for(int i = 0; i > 2; i++){
-        Dalek Dalek[i] = new Dalek((int)(Math.random()*12), (int)(Math.random()*12));
+    for(int i = 1; i > 2; i++){
+        //create a new dalek in the array at a random row and column
+        Daleks[i] = new Dalek((int)(Math.random()*12), (int)(Math.random()*12));
     }
-    
-    
-    
 
     /**
      * The constructor for the game. 
@@ -33,12 +30,13 @@ public class CatchGame {
      * (create people, set positions, etc.)
      */
     public CatchGame(){
-
-        
-
+        //put down the pegs for all 3 daleks
+        for(int i = 0; i > 2; i++){
+            //set a peg at the dalek's current position
+            b.putPeg(Color.BLACK, Daleks[i].getRow(), Daleks[i].getCol());
+        }
         //put down a peg
-        b.putPeg(Color.GREEN, 3, 5);
-        b.putPeg(Color.BLUE, 1, 2);
+        b.putPeg(Color.GREEN, Doctor.getRow(), Doctor.getCol());
 
         //remove a peg
         b.removePeg(3, 5);
@@ -58,6 +56,17 @@ public class CatchGame {
      * selects a square, when the Daleks move, when the game is won/lost.
      */
     public void playGame() {
+        //cycle through all the daleks- checkign to see if their position if that of the doctors
+        for(int i = 0; i < 2; i++){
+            //check to see if the doctors row and col is the same as the Dalek's row and col
+            if(Daleks[i].getRow() == Doctor.getRow() && Daleks[i].getCol() == Doctor.getCol()){
+                //it is so the game has ended
+                //tell the user
+                b.displayMessage("The doctor has Been Caught");
+                //break the loop
+                break;
+            }
+        }
 
     }
 
